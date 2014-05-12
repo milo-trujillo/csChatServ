3ce12681db4fa573b1d103c00482bcdeb7088fa7
milo-trujillo <milo.trujillo@sonomaacademy.org>
2014-05-12 13:30:26 -0700

    Got client reading data from server

--- Client.java
+++ Client.java
@@ -16,6 +16,25 @@ public class Client
 {
 	public static void main( String [] args ) throws IOException
 	{
-		
+		// For now we'll hardcode in the address and port number
+		// but eventually we'll want to get them from the user
+		InetAddress host = InetAddress.getByName("localhost");
+		int port = 8888;
+		try
+		{
+			Socket sock = new Socket(host, port);
+			InputStream in = sock.getInputStream();
+			// The server writes a byte at a time, so we'll read byte by byte
+			byte[] buffer = new byte[1024];
+			int length;
+			while( (length = in.read(buffer)) != -1 ) // -1 == EOF
+			{
+				System.out.write(buffer, 0, length);
+			}
+		}
+		catch( IOException e )
+		{
+			System.out.println("Something went wrong: " + e.getMessage());
+		}
 	}
 }
