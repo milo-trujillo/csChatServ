c49b461dce51895f732cce0744bb2a541bca0d45
milo-trujillo <milo.trujillo@sonomaacademy.org>
2014-05-15 15:10:08 -0700

    Works as a chat server now!

--- Server.hs
+++ Server.hs
@@ -4,49 +4,72 @@
 -}
 
 import System.IO			-- For handles
+import System.Environment	-- For getArgs
 import Network.Socket		-- For sockets
-import Control.Concurrent	-- For threads
+import Control.Concurrent	-- For threads and channels
 
 -- Global vars for configuration
-listen_port = 8888
 max_connections = 30
+listen_port = 8888
 
 -- We'll define messages as (Username, Text)
 type Msg = (String, String)
 
+-- Does some initial setup, then turns over to listenLoop
 main :: IO ()
 main = do
-	sock <- socket AF_INET Stream 0		-- Make new socket
-	setSocketOption sock ReuseAddr 1	-- Make it a reusable listening socket
-	bindSocket sock (SockAddrInet listen_port iNADDR_ANY) -- Bind to dev / port
-	listen sock max_connections 		-- Set max connections
-	listenLoop sock
+	args <- getArgs
+	if length args == 1 && isInteger (args !! 0)
+	then
+		do
+		-- listen_port <- read (args !! 0) :: Int -- This bit isn't working yet
+		msgs <- newChan						-- Stores all messages
+		sock <- socket AF_INET Stream 0		-- Make new socket
+		setSocketOption sock ReuseAddr 1	-- Set reusable listening socket
+		bindSocket sock (SockAddrInet listen_port iNADDR_ANY) -- Bind any dev
+		listen sock max_connections 		-- Set max connections
+		listenLoop sock msgs
+	else
+		putStrLn "Usage: Server <port number>"
 
--- This may cause a stack overflow given enough time, I need to look into
--- how Haskell optimizes recursion
-listenLoop :: Socket -> IO ()
-listenLoop servSock = do
+-- Listens for a new client, then forks off a handler
+listenLoop :: Socket -> Chan Msg -> IO ()
+listenLoop servSock msgs = do
 	client <- accept servSock
-	forkIO (handle client) -- Run 'handle' on a background thread
-	listenLoop servSock
+	forkIO (handle client msgs) -- Run 'handle' on a background thread
+	listenLoop servSock msgs
 
 -- 'accept' returns a tuple of a socket and the address it's connected on
 -- We're only interested in the socket right now
-handle :: (Socket, SockAddr) -> IO ()
-handle (sock, _) = do
+handle :: (Socket, SockAddr) -> Chan Msg -> IO ()
+handle (sock, _) msgs = do
 	s <- socketToHandle sock ReadWriteMode -- convert the socket to a handle
 	hSetBuffering s NoBuffering -- Write byte by byte over the network
-	-- Now let's test IO
 	hPutStr s "Your name: "
 	name <- hGetLine s
 	hPutStrLn s ("Hello " ++ name)
-	hPutStrLn s "We're echoing you now."
-	echo s
+	hPutStrLn s "Welcome to the Server."
+	write <- dupChan msgs
+	read <- dupChan msgs
+	forkIO (readUser name s write)
+	readMsgs s read -- Do _not_ fork this line! We don't want hClose to run!
 	hClose s -- This closes the handle _and_ the socket
 
-echo :: Handle -> IO ()
-echo sock = do
+-- This reads from the user and appends new messages to the global queue
+readUser :: String -> Handle -> Chan Msg -> IO ()
+readUser user sock msgs = do
 	msg <- hGetLine sock
-	hPutStrLn sock msg
-	echo sock
-	
+	writeChan msgs (user, msg)
+	readUser user sock msgs
+
+-- This reads from the message queue and prints results over socket to user
+readMsgs :: Handle -> Chan Msg -> IO ()
+readMsgs sock msgs = do
+	(user, msg) <- readChan msgs
+	hPutStrLn sock ("<" ++ user ++ "> " ++ msg)
+	readMsgs sock msgs
+
+-- Checks if a string contains only an integer
+isInteger s = case reads s :: [(Integer, String)] of
+	[(_, "")] -> True
+	_         -> False
